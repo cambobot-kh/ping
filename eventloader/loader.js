223fc3526f29ffae7c05a263f6ce7fa341bbc1f4
healong <37354105+healong@users.noreply.github.com>
2018-04-21 13:04:09 +0700

    Create loader.js

---
+++ eventloader/loader.js
@@ -0,0 +1,14 @@
+// just a small little function to make life a bit easier
+function load_event(event) {
+  return require("../events/" + event);
+}
+
+module.exports = {
+  run : Client => {
+    // just discord events, loading their functions from their specified files
+    Client.on("message", msg => { load_event("message").run(msg, Client); });
+    Client.on("ready", () => { load_event("ready").run(); });
+    Client.on("disconnect", () => { load_event("disconnect").run(); });
+    Client.on("reconnecting", () => { load_event("reconnecting").run(); });
+  }
+}
